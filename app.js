ff67eb84d9b1e01dee9f6ecc5a27b9cd68c484db
Dan Jenkins <dan.jenkins88@gmail.com>
2019-11-13 14:08:53 +0000

    More formatting changes (sorry Dave!)

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const Srf = require('drachtio-srf') ;
-const srf = new Srf() ;
+const Srf = require('drachtio-srf');
+const srf = new Srf();
 const logger = require('pino')();
 const Register = require('./lib/register');
 const Registrar = require('./lib/registrar');
 const Subscriber = require('./lib/subscriber');
 const CallProcessor = require('./lib/call-processor');
-const registrar = new Registrar(logger) ;
-const register = new Register(logger) ;
-const subscriber = new Subscriber(logger) ;
-const callProcessor = new CallProcessor(logger) ;
-const config = require('config') ;
-const Client = require('rtpengine-client').Client ;
+const registrar = new Registrar(logger);
+const register = new Register(logger);
+const subscriber = new Subscriber(logger);
+const callProcessor = new CallProcessor(logger);
+const config = require('config');
+const Client = require('rtpengine-client').Client;
 const rtpengine = new Client(config.get('rtpengine.local-port'));
 
 if (config.get('drachtio.mode') === 'outbound') {
@@ -18,10 +18,10 @@ if (config.get('drachtio.mode') === 'outbound') {
 } else {
   srf.connect(config.get('drachtio'))
   .on('connect', (err, hostport) => {
-    console.log(`connected to drachtio listening for SIP on hostport ${hostport}`) ;
+    console.log(`connected to drachtio listening for SIP on hostport ${hostport}`);
   })
   .on('error', (err) => {
-    console.error(`Error connecting to drachtio server: ${err.message}`) ;
+    console.error(`Error connecting to drachtio server: ${err.message}`);
   });
 }
 
